1a3795552eb5fbe67bd3bbb6335ca712e67df5c4
egistafatmawati <egistafatmawati72@gmail.com>
2024-12-13 11:21:48 +0700

    Create aplikasi.java

---
+++ aplikasi.java
@@ -0,0 +1,23 @@
+package aplikasi;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Aplikasi extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("FXMLDocument.fxml"));
+        Scene scene = new Scene(loader.load());
+        
+        primaryStage.setTitle("GAME ULAR");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
