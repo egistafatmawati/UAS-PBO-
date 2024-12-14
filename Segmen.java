0f5c54734c8f25cb8c35bcae7b60758ca1d4a0cb
egistafatmawati <egistafatmawati72@gmail.com>
2024-12-14 22:11:26 +0700

    Update Segmen.java

--- Segmen.java
+++ Segmen.java
@@ -1,23 +1,18 @@
 package aplikasi;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
 
-public class Aplikasi extends Application {
-
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("FXMLDocument.fxml"));
-        Scene scene = new Scene(loader.load());
-        
-        primaryStage.setTitle("GAME ULAR");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+public class Segmen extends GameObject {
+    public Segmen(int x, int y) {
+        super(x, y);
     }
 
-    public static void main(String[] args) {
-        launch(args);
+    @Override
+    public void render(GraphicsContext gc) {
+        gc.setFill(Color.LIGHTGREEN);
+        gc.fillRect(getX() * 25, getY() * 25, 24, 24);
+        gc.setFill(Color.GREEN);
+        gc.fillRect(getX() * 25 + 1, getY() * 25 + 1, 22, 22);
     }
 }
